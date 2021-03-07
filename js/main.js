20db715eeef336aeb2c8d00be0665a4f74aa7d77
Zrybea Whitfield <66852175+ZrybeaWhitfield@users.noreply.github.com>
2021-03-06 22:28:56 -0500

    fianl draft, simple api2

--- js/main.js
+++ js/main.js
@@ -1,22 +1,31 @@
 document.querySelector("button").addEventListener("click", getCharacter)
 
 function getCharacter(){
-  let name = document.querySelector("input").value
-  let url = `https://rickandmortyapi.com/api/character/?name=${name}`
+  let characterName = document.querySelector("input").value
+  let names = document.querySelector(".names")
+  let statuses = document.querySelector(".status")
+  let images = document.querySelector(".images")
+  let url = `https://rickandmortyapi.com/api/character/?name=${characterName}`
 
   fetch(url)
     .then(res => res.json())
     .then(data =>{
       for(i=0; i< data.results.length; i++){
-        console.log(data.results[i].name)
-        document.querySelector(".searchResults").innerText += `${data.results[i].name}\n`
-        document.querySelector(".searchResults").addEventListener("click", event =>{
-          if(event.target.className === "searchResults"){
-            // get help for This
-            //let clickedName =
-            // document.querySelector("h2").innerText = `${clickedName}`
-          }
-        })
+        console.log(data)
+        let listItem = document.createElement("li")
+        let listItem2 = document.createElement("li")
+        let listItem3 = document.createElement("img")
+        names.appendChild(listItem)
+        statuses.appendChild(listItem2)
+        images.appendChild(listItem3)
+
+        listItem.innerText = data.results[i].name
+        listItem2.innerText = data.results[i].status
+        listItem3.src = data.results[i].image
+        // document.querySelector(".searchResults").innerText += `${data.results[i].name}\n`
+        // document.querySelector(".searchResults").addEventListener("click", event =>{
+        //
+        // })
 
 
       }
